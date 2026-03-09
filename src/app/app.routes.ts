fb1f0db8483b9e888c447aacf509e98ca9fb54a4
karan-technous <karan@technous.in>
2026-03-09 11:42:17 +0530

    init commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Notification } from './pages/notification/notification';
+import { MyTasks } from './pages/my-tasks/my-tasks';
+import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => Home,
+  },
+  {
+    path: 'login',
+    loadComponent: () => Login,
+  },
+  {
+    path: 'register',
+    loadComponent: () => Register,
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => Dashboard,
+  },
+  {
+    path: 'notification',
+    loadComponent: () => Notification,
+  },
+  {
+    path: 'my-tasks',
+    loadComponent: () => MyTasks,
+  },
+];
