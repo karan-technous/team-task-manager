106cae66ee2d2b8facf4ff2d705c6d257b363b4b
karan-technous <karan@technous.in>
2026-03-11 10:57:48 +0530

    ui fixe

---
+++ src/app/common/popup-dialoge/popup-dialoge.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-popup-dialoge',
+  imports: [],
+  templateUrl: './popup-dialoge.html',
+  styleUrl: './popup-dialoge.scss',
+})
+export class PopupDialoge {
+
+}
